497319581c28684cd6b3bfcfe02fe0b14fcb51ea
Sai Kushal Polisetty <Saikushal.Polisetty@Resideo.com>
2020-06-22 18:23:29 +0530

    Added connection id logging

--- SignalRDemo/Hubs/ChatHub.cs
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -10,6 +10,7 @@ namespace SignalRDemo.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
+            Console.WriteLine($"{Context.ConnectionId}");
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
